b5fb24e656e3c38ea95ea07ebd0193aa32b68cd2
Nick Echols <nickechols@Nicks-iMac.home>
2023-01-23 19:07:22 -0600

    added comments

--- challengeTypescript.ts
+++ challengeTypescript.ts
@@ -3,6 +3,7 @@
     let maxSum = arr[0];
     let currentSum = arr[0];
 
+    // For Loop
     for (let i = 1; i < arr.length; i++) {
         currentSum = Math.max(arr[i], currentSum + arr[i]);
         maxSum = Math.max(maxSum, currentSum);
