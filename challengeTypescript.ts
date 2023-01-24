ec1ca21a884f25861c5ed8d57f511f0383e39309
Nick Echols <nickechols@Nicks-iMac.home>
2023-01-23 19:00:13 -0600

    added ReadMe and Challenge 1 for Typescript

---
+++ challengeTypescript.ts
@@ -0,0 +1,12 @@
+  //Challenge 1 for Typescript Challenge
+  function findLargestContinuousSum(arr: number[]): number {
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+};
