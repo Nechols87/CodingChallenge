6dbe4573ecde3552d3174368e584fea115b67445
Nick Echols <nickechols@Nicks-iMac.home>
2023-01-23 19:04:57 -0600

    added in documentation notes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
+// Generates a random string based on the length of the string that is inputed
 function generateRandomString(length: number): string {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -12,10 +13,12 @@ function generateRandomString(length: number): string {
     return result;
   }
 
+// Handles get request with possible query param length from front end and sends back result. Defaults to 5
 app.get('/', (req: Request, res: Response ) => {
     const { length } = req.query
     generateRandomString(Number(length))
     res.send(generateRandomString(Number(length || 5)));
 });
 
+// Make sure server is running
 app.listen(3000, () => console.log('server running'))
